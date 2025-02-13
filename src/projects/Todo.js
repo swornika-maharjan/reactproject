e78f37ccac6e68a9288b3c736fb829467193c957
swornika-maharjan <swornikamhrjn@gmail.com>
2025-02-13 21:41:11 +0545

    updated

--- src/projects/Todo.js
+++ src/projects/Todo.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Todo.css"; 
+import "./Todo.css";
 
 const TodoApp = () => {
   const [tasks, setTasks] = useState([]);
@@ -18,30 +18,39 @@ const TodoApp = () => {
     setTasks(updatedTasks);
   };
 
+  const markAllCompleted = () => {
+    const updatedTasks = tasks.map(task => ({ ...task, completed: true }));
+    setTasks(updatedTasks);
+  };
+
   const removeTask = (index) => {
     setTasks(tasks.filter((_, i) => i !== index));
   };
 
   return (
-    <div className="app">
-      <h1>To-Do List</h1>
-      <div className="input-container">
-        <input
-          type="text"
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          placeholder="Add a new task"
-        />
-        <button onClick={addTask}>Add</button>
+    <div className="container">
+      <div className="app">
+        <h1>To-Do List</h1>
+        <div className="input-container">
+          <input
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            placeholder="Add a new task"
+          />
+          <button onClick={addTask}>Add</button>
+        </div>
+        <button onClick={markAllCompleted} className="mark-all">Mark All Completed</button>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index} className={task.completed ? "completed" : ""}>
+              <input type="checkbox" checked={task.completed} onChange={() => toggleTask(index)} />
+              <span onClick={() => toggleTask(index)}>{task.text}</span>
+              <button onClick={() => removeTask(index)}>X</button>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index} className={task.completed ? "completed" : ""}>
-            <span onClick={() => toggleTask(index)}>{task.text}</span>
-            <button onClick={() => removeTask(index)}>X</button>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
